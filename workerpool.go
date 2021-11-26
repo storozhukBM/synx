8db522ace8870652b11b7f27d107b0a3cc6cadd4
bohdanstorozhuk <storozhuk.b.m@gmail.com>
2021-11-26 14:09:28 +0000

    Do not block worker pool and reduce task waiting time depending on number of waiting workers.

--- workerpool.go
+++ workerpool.go
@@ -1,47 +1,80 @@
 package synx
 
 import (
+	"math"
+	"sync/atomic"
 	"time"
 )
 
 type WorkerPool struct {
-	lifetime  time.Duration
-	semaphore chan struct{}
-	taskQueue chan func()
+	maxWaitingWorkers     int32
+	maxWaitingTime        time.Duration
+	taskQueue             chan func()
+	currentWaitingWorkers int32
 }
 
-func NewWorkerPool(maxWorkers int, lifetime time.Duration) *WorkerPool {
+func NewWorkerPool(maxWaitingWorkers int, maxWaitingTime time.Duration) *WorkerPool {
+	switch {
+	case maxWaitingWorkers < 1 && maxWaitingWorkers < math.MaxInt32:
+		panic("synx: maxWaitingWorkers should be between 1 and math.MaxInt32")
+	case maxWaitingTime < 1:
+		panic("synx: maxWaitingTime should be greater than zero")
+	}
+
 	return &WorkerPool{
-		lifetime:  lifetime,
-		semaphore: make(chan struct{}, maxWorkers),
-		taskQueue: make(chan func()),
+		maxWaitingWorkers: int32(maxWaitingWorkers),
+		maxWaitingTime:    maxWaitingTime,
+		taskQueue:         make(chan func()),
 	}
 }
 
 func (wp *WorkerPool) Do(task func()) {
 	select {
 	case wp.taskQueue <- task:
-		// submitted, everything is ok
-
-	case wp.semaphore <- struct{}{}:
+	default:
 		go wp.startWorker(task)
 	}
 }
 
-func (wp *WorkerPool) startWorker(task func()) {
-	defer func() { <-wp.semaphore }()
+func (wp *WorkerPool) CurrentWaitingWorkers() int {
+	return int(atomic.LoadInt32(&wp.currentWaitingWorkers))
+}
 
+func (wp *WorkerPool) startWorker(task func()) {
 	task() // do the given task
 
-	tick := time.NewTicker(wp.lifetime)
+	tick := time.NewTicker(wp.maxWaitingTime)
 	defer tick.Stop()
 
-	for {
+	workerLoop := func() (shouldContinue bool) {
+		currentWaitingWorkers := atomic.AddInt32(&wp.currentWaitingWorkers, 1)
+		defer atomic.AddInt32(&wp.currentWaitingWorkers, -1)
+
+		if currentWaitingWorkers > wp.maxWaitingWorkers {
+			return false // too many workers already waiting
+		}
+
+		// try to get task without blocking
 		select {
 		case t := <-wp.taskQueue:
 			t()
-			tick.Reset(wp.lifetime)
+			return true
+		default:
+		}
+
+		// wait for task, but the more workers we have - the shorter wait time gets
+		tick.Reset(time.Duration(wp.maxWaitingTime.Nanoseconds() / (int64(currentWaitingWorkers) * int64(currentWaitingWorkers))))
+		select {
+		case t := <-wp.taskQueue:
+			t()
+			return true
 		case <-tick.C:
+			return false
+		}
+	}
+
+	for {
+		if shouldContinue := workerLoop(); !shouldContinue {
 			return
 		}
 	}
--- workerpool_test.go
+++ workerpool_test.go
@@ -1,64 +1,85 @@
 package synx
 
 import (
+	"fmt"
 	"runtime"
+	"strings"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
 func TestWorkerPool(t *testing.T) {
+	const maxWaitingTime = time.Second
 	const shortDelay = 100 * time.Millisecond
 	const workers = 3
-	wp := NewWorkerPool(workers, time.Second)
+	wp := NewWorkerPool(workers, maxWaitingTime)
 
 	// submit tasks and grow till `workers` goroutines
 	startG := runtime.NumGoroutine()
 
-	for i := 1; i <= workers; i++ {
+	// create twice more workers than expected
+	for i := 1; i <= 2*workers; i++ {
 		wp.Do(func() {
 			time.Sleep(shortDelay)
 		})
+
+		if wp.CurrentWaitingWorkers() != 0 {
+			t.Fatalf("some workers already waiting")
+		}
 
 		if n := runtime.NumGoroutine() - startG; n != i {
 			t.Fatalf("want %v, got %v", i, n)
 		}
 	}
 
-	// workers are alive so blocking and waiting to finish
-
-	for i := 1; i <= workers; i++ {
-		startG = runtime.NumGoroutine()
-		wp.Do(func() {
-			time.Sleep(shortDelay)
-		})
-
-		if n := runtime.NumGoroutine() - startG; n != 0 {
-			t.Fatalf("want %v, got %v", i, n)
-		}
+	// wait for tasks to finish and check that only max waiting workers are still waiting for new tasks
+	time.Sleep(time.Duration(float32(shortDelay.Nanoseconds()) * 1.2))
+	waitingWorkers := wp.CurrentWaitingWorkers()
+	if waitingWorkers != workers {
+		t.Fatalf("want %v, got %v", workers, waitingWorkers)
 	}
-}
-
-func TestWorkerPool_Lifetime(t *testing.T) {
-	const shortDelay = 100 * time.Millisecond
-	const longDelay = 300 * time.Millisecond
-	const workers = 3
-
-	wp := NewWorkerPool(workers, shortDelay)
-	startG := runtime.NumGoroutine()
-
-	for i := 1; i <= workers; i++ {
-		wp.Do(func() {
-			time.Sleep(longDelay)
-		})
-	}
-
 	if n := runtime.NumGoroutine() - startG; n != workers {
 		t.Fatalf("want %v, got %v", workers, n)
 	}
 
-	time.Sleep(2 * longDelay)
-
+	// wait for workers to give up and exit
+	time.Sleep(maxWaitingTime)
+	waitingWorkers = wp.CurrentWaitingWorkers()
+	if waitingWorkers != 0 {
+		t.Fatalf("want %v, got %v", 0, waitingWorkers)
+	}
 	if n := runtime.NumGoroutine() - startG; n != 0 {
 		t.Fatalf("want %v, got %v", 0, n)
+	}
+}
+
+func TestExponentialDeclineOfWaitingTime(t *testing.T) {
+	currentlyWorking := int64(0)
+	wp := NewWorkerPool(128, 10*60*time.Second)
+	for i := 0; i < 200; i++ {
+		wp.Do(func() {
+			atomic.AddInt64(&currentlyWorking, 1)
+			defer atomic.AddInt64(&currentlyWorking, -1)
+			time.Sleep(5 * time.Millisecond)
+		})
+	}
+	go func() {
+		for {
+			wp.Do(func() {
+				atomic.AddInt64(&currentlyWorking, 1)
+				defer atomic.AddInt64(&currentlyWorking, -1)
+				time.Sleep(time.Millisecond)
+			})
+			time.Sleep(1000 * time.Microsecond)
+		}
+	}()
+	for i := 0; i < 1000; i++ {
+		fmt.Printf(
+			"%.4d ms: %.4d/%.4d %s\n",
+			i, atomic.LoadInt64(&currentlyWorking), wp.CurrentWaitingWorkers(),
+			strings.Repeat("*", int(atomic.LoadInt64(&currentlyWorking)))+strings.Repeat("-", (wp.CurrentWaitingWorkers())),
+		)
+		time.Sleep(time.Millisecond)
 	}
 }
